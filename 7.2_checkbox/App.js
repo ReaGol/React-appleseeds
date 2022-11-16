ab4008deed873af3c75a4f380f224a23bdea0737
ReaGol <reutgoldin@gmail.com>
2022-11-16 12:40:31 +0200

    exs

---
+++ 7.2_checkbox/App.js
@@ -0,0 +1,23 @@
+// define component of checkbox that can receive in props what it says and what is its 
+// defaultive value and use useState to save it and update
+// import { useState } from 'react'
+// import React from 'react'
+
+// const Checkbox = (props) => {
+//     return (
+//       <div>
+//         <input type='checkbox' />
+//         <label>{props.checkboxText}</label>
+//       </div>
+//     );
+// }
+// function App() {
+//   return (
+//     <div>
+//         <Checkbox/>
+//     </div>
+//   )
+// }
+
+// export default App
+
